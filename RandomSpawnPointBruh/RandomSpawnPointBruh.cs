8859d4cc88968017b3ffd26e6711c850d8630777
Vapok <vapokrocks@gmail.com>
2026-09-16 19:31:42 -0400

    Release v2.0.4: Updated README with Telemetry Information

--- RandomSpawnPointBruh/RandomSpawnPointBruh.cs
+++ RandomSpawnPointBruh/RandomSpawnPointBruh.cs
@@ -29,7 +29,7 @@ namespace RandomSpawnPointBruh
         //Module Constants
         private const string _pluginId = "vapok.mods.RandomSpawnPointBruh";
         private const string _displayName = "RandomSpawnPointBruh";
-        private const string _version = "2.0.3";
+        private const string _version = "2.0.4";
         
         //Interface Properties
         public string PluginId => _pluginId;
