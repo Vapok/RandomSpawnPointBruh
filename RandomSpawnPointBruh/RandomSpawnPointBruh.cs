c33236f61ab8606712bc815d4ad54f4562b693ba
Vapok <vapokrocks@gmail.com>
2026-09-16 18:12:44 -0400

    Release v2.0.3: Unified Splash Screen & Telemetry Controls

--- RandomSpawnPointBruh/RandomSpawnPointBruh.cs
+++ RandomSpawnPointBruh/RandomSpawnPointBruh.cs
@@ -29,7 +29,7 @@ namespace RandomSpawnPointBruh
         //Module Constants
         private const string _pluginId = "vapok.mods.RandomSpawnPointBruh";
         private const string _displayName = "RandomSpawnPointBruh";
-        private const string _version = "2.0.2";
+        private const string _version = "2.0.3";
         
         //Interface Properties
         public string PluginId => _pluginId;
