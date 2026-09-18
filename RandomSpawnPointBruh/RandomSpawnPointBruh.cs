318055d7e62c03a1dfab76026f832a0bf73afdcc
Vapok <vapokrocks@gmail.com>
2026-09-18 06:33:15 -0400

    Release v2.0.6: Splash Window Updates & Valheim 1.0.14 Alignment

--- RandomSpawnPointBruh/RandomSpawnPointBruh.cs
+++ RandomSpawnPointBruh/RandomSpawnPointBruh.cs
@@ -29,7 +29,7 @@ namespace RandomSpawnPointBruh
         //Module Constants
         private const string _pluginId = "vapok.mods.RandomSpawnPointBruh";
         private const string _displayName = "RandomSpawnPointBruh";
-        private const string _version = "2.0.5";
+        private const string _version = "2.0.6";
         
         //Interface Properties
         public string PluginId => _pluginId;
